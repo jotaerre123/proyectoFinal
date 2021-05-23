896bb0f7c7186fc0f88ca778827fb21a779264ad
jotaerre123 <74652146+jotaerre123@users.noreply.github.com>
2021-05-23 12:44:10 +0200

    Delete ProyectoFinalJaimeJimenez directory

--- ProyectoFinalJaimeJimenez/src/main/java/com/salesianostriana/dam/proyectofinaljaimejimenez/repositorio/ComplementoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectofinaljaimejimenez.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectofinaljaimejimenez.modelo.Complemento;
-
-public interface ComplementoRepository extends JpaRepository<Complemento, Long>{
-
-}
